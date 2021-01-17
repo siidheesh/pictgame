5d7e3263d7ac17cefce0011de1ceff991e7ce312
siidheesh <siidheesh@gmail.com>
2021-01-17 15:49:02 +0800

    added fsm for matching

--- front/src/index.js
+++ front/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App.tsx';
 import Canvas from './Canvas.tsx';
 import Home from './Home';
+import Main from './Main';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -16,4 +17,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
